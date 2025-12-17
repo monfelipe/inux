3234e30b50c58c72bf7393eb4f45f982f6de5351
monfelipe <felipedamatarodrigues@proton.me>
2025-12-17 11:09:48 -0300

    adiciona nova janela

--- main.c
+++ main.c
@@ -1,42 +1,51 @@
 #include <ncurses.h>
 #include <unistd.h>
 
-void mover_janela(WINDOW *win, int *pos_y, int *pos_x, int tecla) {
+void mover_janela(WINDOW *win, WINDOW *win2,
+                  int *pos_y, int *pos_x, int tecla) {
     int nova_y = *pos_y;
     int nova_x = *pos_x;
 
-    switch(tecla) {
+    switch (tecla) {
         case KEY_UP:
-            if(nova_y > 2) nova_y--;
+            if (nova_y > 2) nova_y--;
             break;
         case KEY_DOWN:
-            if(nova_y < LINES - 9) nova_y++;
+            if (nova_y < LINES - 9) nova_y++;
             break;
         case KEY_LEFT:
-            if(nova_x > 0) nova_x--;
+            if (nova_x > 0) nova_x--;
             break;
         case KEY_RIGHT:
-            if(nova_x < COLS - 31) nova_x++;  // COLS - largura da janela
+            if (nova_x < COLS - 31) nova_x++;
             break;
     }
 
-    // Atualiza posição
     *pos_y = nova_y;
     *pos_x = nova_x;
 
     mvwin(win, nova_y, nova_x);
-
     clear();
+
     attron(A_BOLD);
-    mvprintw(0, 2, "Janelas! Use as SETAS para mover");
+    mvprintw(0, 2, "Inux");
     attroff(A_BOLD);
     refresh();
+
+    if (win2 != NULL) {
+        touchwin(win2);
+        wrefresh(win2);
+    }
+
+    touchwin(win);
     wrefresh(win);
 }
 
 int main() {
     WINDOW *win1;
+    WINDOW *win2 = NULL;
     int win_y = 3, win_x = 2;
+    int ch, contador = 0;
 
     initscr();
     start_color();
@@ -47,24 +56,24 @@ int main() {
     init_pair(1, COLOR_WHITE, COLOR_BLUE);
 
     attron(A_BOLD);
-    mvprintw(0, 2, "Janelas! Use as SETAS para mover");
+    mvprintw(0, 2, "Inux");
     attroff(A_BOLD);
     refresh();
 
     win1 = newwin(8, 30, win_y, win_x);
     wbkgd(win1, COLOR_PAIR(1));
     box(win1, 0, 0);
-    mvwprintw(win1, 0, 11, "Quase OS");
+    mvwprintw(win1, 0, 11, "Inux");
     wrefresh(win1);
 
-    int ch, contador = 0;
-    while((ch = getch()) != 'q') {
-        switch(ch) {
+    while ((ch = getch()) != 'q') {
+        switch (ch) {
+
             case KEY_UP:
             case KEY_DOWN:
             case KEY_LEFT:
             case KEY_RIGHT:
-                mover_janela(win1, &win_y, &win_x, ch);
+                mover_janela(win1, win2, &win_y, &win_x, ch);
                 break;
 
             case '1':
@@ -79,8 +88,37 @@ int main() {
                 mvwprintw(win1, 1, 2, "Janela limpa!");
                 wrefresh(win1);
                 break;
+
+            case 'n':
+                if (win2 == NULL) {
+                    win2 = newwin(8, 30, 30, 50);
+                    wbkgd(win2, COLOR_PAIR(1));
+                    box(win2, 0, 0);
+                    mvwprintw(win2, 0, 6, "Bem-vindo ao Inux");
+                    touchwin(win2);
+                    wrefresh(win2);
+                }
+                break;
+            case 'x':
+                if (win2 != NULL) {
+                    werase(win2);
+                    wrefresh(win2);
+                    delwin(win2);
+                    win2 = NULL;
+                    clear();
+                    attron(A_BOLD);
+                    mvprintw(0, 2, "Inux");
+                    attroff(A_BOLD);
+                    refresh();
+                    touchwin(win1);
+                    wrefresh(win1);
+                }
+                break;
         }
     }
+
+    if (win2 != NULL)
+        delwin(win2);
 
     delwin(win1);
     endwin();
