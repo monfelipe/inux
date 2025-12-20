13c994d6cb10dae86e969af0df1e15e62fb53deb
monfelipe <felipedamatarodrigues@proton.me>
2025-12-20 10:45:07 -0300

    Otimização de código

--- main.c
+++ main.c
@@ -1,32 +1,32 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-void mover_janela(WINDOW *win, WINDOW *win2,
-                  int *pos_y, int *pos_x, int tecla) {
+void move_win(WINDOW *win, WINDOW *win2,
+              int *pos_y, int *pos_x, int tecla) {
     int nova_y = *pos_y;
     int nova_x = *pos_x;
 
     switch (tecla) {
         case KEY_UP:
-            if (win2 == NULL){
-                if (nova_y > 2) nova_y--;
-            }
+            if (win2 == NULL && nova_y > 2)
+                nova_y--;
             break;
+
         case KEY_DOWN:
-            if (win2 == NULL){
-                if (nova_y < LINES - 9) nova_y++;
-            }
+            if (win2 == NULL && nova_y < LINES - 9)
+                nova_y++;
             break;
+
         case KEY_LEFT:
-            if (win2 == NULL){
-                if (nova_x > 0) nova_x--;
-            }
+            if (win2 == NULL && nova_x > 0)
+                nova_x--;
             break;
+
         case KEY_RIGHT:
-            if (win2 == NULL){
-                if (nova_x < COLS - 31) nova_x++;
-            }
+            if (win2 == NULL && nova_x < COLS - 31)
+                nova_x++;
             break;
     }
 
@@ -34,7 +34,8 @@ void mover_janela(WINDOW *win, WINDOW *win2,
     *pos_x = nova_x;
 
     mvwin(win, nova_y, nova_x);
-    clear();
+
+    erase();  // melhor que clear()
 
     attron(A_BOLD);
     mvprintw(0, 2, "Inux System");
@@ -51,16 +52,21 @@ void mover_janela(WINDOW *win, WINDOW *win2,
 }
 
 int main() {
-    int porc = 2;
+    int pount;
     FILE *arquivo;
     char linha[256];
 
     arquivo = fopen("aplications.inux", "r");
+    if (!arquivo) {
+        perror("Erro ao abrir aplications.inux");
+        return 1;
+    }
 
     WINDOW *win1;
     WINDOW *win2 = NULL;
+
     int win_y = 3, win_x = 2;
-    int ch, contador = 0;
+    int ch;
 
     initscr();
     start_color();
@@ -88,58 +94,61 @@ int main() {
             case KEY_DOWN:
             case KEY_LEFT:
             case KEY_RIGHT:
-                mover_janela(win1, win2, &win_y, &win_x, ch);
+                move_win(win1, win2, &win_y, &win_x, ch);
                 break;
 
             case 'c':
-                wclear(win1);
+                werase(win1);
                 box(win1, 0, 0);
                 mvwprintw(win1, 1, 2, "Isso nao existe mais...");
                 wrefresh(win1);
                 break;
 
-                case 'n':
-                    if (win2 == NULL) {
-                        porc = 1;          // reinicia linha
-                        rewind(arquivo);   // volta ao início do arquivo
+            case 'n':
+                if (win2 == NULL) {
+                    rewind(arquivo);
+                    pount = 1;
 
-                        win2 = newwin(8, 30, 30, 50);
-                        wbkgd(win2, COLOR_PAIR(1));
-                        box(win2, 0, 0);
+                    win2 = newwin(8, 30, 30, 50);
+                    wbkgd(win2, COLOR_PAIR(1));
+                    box(win2, 0, 0);
+                    mvwprintw(win2, 0, 4, " Bem-vindo ao Inux ");
 
-                        mvwprintw(win2, 0, 4, " Bem-vindo ao Inux ");
+                    while (fgets(linha, sizeof(linha), arquivo)) {
+                        if (pount > 6)
+                            break;
 
-                        while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-                            if (porc >= 7) break;
-                            mvwprintw(win2, porc, 1, "%s", linha);
-                            porc++;
-                        }
-
-                        wrefresh(win2);
-                    } else {
-                        werase(win2);
-                        wrefresh(win2);
-                        delwin(win2);
-                        win2 = NULL;
-
-                        clear();
-                        attron(A_BOLD);
-                        mvprintw(0, 2, "Inux System");
-                        attroff(A_BOLD);
-                        refresh();
-                        touchwin(win1);
-                        wrefresh(win1);
+                        linha[strcspn(linha, "\n")] = '\0';
+                        mvwprintw(win2, pount, 1, "%s", linha);
+                        pount++;
                     }
-                    break;
 
+                    wrefresh(win2);
+
+                } else {
+                    werase(win2);
+                    wrefresh(win2);
+                    delwin(win2);
+                    win2 = NULL;
+
+                    erase();
+                    attron(A_BOLD);
+                    mvprintw(0, 2, "Inux System");
+                    attroff(A_BOLD);
+                    refresh();
+
+                    touchwin(win1);
+                    wrefresh(win1);
+                }
+                break;
         }
     }
 
-    if (win2 != NULL)
+    if (win2)
         delwin(win2);
 
-    fclose(arquivo);
     delwin(win1);
+    fclose(arquivo);
     endwin();
     return 0;
 }
